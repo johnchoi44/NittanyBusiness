5a7ba0c1029aa66bb7b9208b58a98f5760e387e8
Khavab <shiblidnomani@gmail.com>
2025-04-28 05:57:06 -0400

    Checkout page css update

--- frontend/src/pages/Checkout.js
+++ frontend/src/pages/Checkout.js
@@ -25,25 +25,25 @@ const CheckOut = () => {
             return null;
         }
         fetchCreditCardInfo();
-    }, [navigate, state, userEmail]);
+    }, [navigate, state, userEmail, useNew]);
 
     if (!state?.order) return null;
 
-    const { date, listing_id, seller_email, buyer_email, product_title, unit_price, quantity, total_price } = state.order;
+    const { date, listing_id, seller_email, quantity, total_price } = state.order;
 
     const fetchCreditCardInfo = async () => {
         try {
             const res = await axios.get("http://localhost:8080/credit-cards", {
                 params: { owner_email: userEmail }
             });
-            
+
             setCards(res.data.cards || []);
             if (res.data.cards.length > 0) {
                 setSelectedCard(res.data.cards[0].credit_card_num);
                 console.log("Credit card info fetched successfully")
             } else {
                 setUseNew(true);
-            } 
+            }
         } catch (err) {
             setMessage("Error: failed to load credit card information.");
         }
@@ -63,10 +63,10 @@ const CheckOut = () => {
 
     const handleCVV = (e) => {
         let input = e.target.value;
-        input = input.slice(0,3);
+        input = input.slice(0, 3);
         setCVV(input);
     };
-    
+
     const handleConfirm = async (e) => {
         e?.preventDefault();
         // build credit_card object either from saved or from form
@@ -82,13 +82,13 @@ const CheckOut = () => {
 
         try {
             await axios.post("http://localhost:8080/checkout", {
-            seller_email,
-            listing_id,
-            buyer_email: userEmail,
-            date: new Date().toISOString().slice(0, 10),
-            quantity,
-            payment: total_price,
-            credit_card,
+                seller_email,
+                listing_id,
+                buyer_email: userEmail,
+                date: new Date().toISOString().slice(0, 10),
+                quantity,
+                payment: total_price,
+                credit_card,
             });
             navigate("/order-management");
         } catch (err) {
@@ -98,102 +98,108 @@ const CheckOut = () => {
 
     return (
         <>
-        <NavBar />
-        <div className="checkout-container">
-            <div className="order-preview-container"> 
-                <button id="order-back-btn" onClick={() => navigate(-1)}>&#8592;</button>
+            <NavBar />
+            <div className="checkout-container">
+                <div className="order-preview-container">
+                    <button id="order-back-btn" onClick={() => navigate(-1)}>&#8592;</button>
 
-                <h1 id="order-review-title">Select Your Payment Method</h1>
+                    <h1 id="order-review-title">Select Your Payment Method</h1>
 
-                <div className="divider" />
+                    <div className="divider" />
 
-                <div className="payment-list-container">
-                    {cards.length > 0 && !useNew && (
-                        <div className="card-radio-container">
-                            <p>Saved Credit Card</p>
 
-                            {cards.map((c) => (
-                                <label key={c.credit_card_num} className="card-radio-option">
-                                    <input
-                                    type="radio"
-                                    name="card"
-                                    value={c.credit_card_num}
-                                    checked={selectedCard === c.credit_card_num}
-                                    onChange={(e) => setSelectedCard(e.target.value)}
-                                    />
-                                    {c.card_type} ••••{c.credit_card_num.slice(-4)}
-                                </label>
-                            ))}
-                        </div>
-                    )}
-                    
-                    <button id="new-btn" onClick={handleClick}>Use New Credit Card</button>
-                    {useNew && (
-                        <div>
-                            <form onSubmit={handleConfirm}>
-                                <fieldset>
-                                    <div className="form-select">
-                                        <p className="form-label">Select Card Type</p>
-                                        <select
-                                            value={cardType}
-                                            onChange={e => setCardType(e.target.value)}
+
+                    <div className="payment-list-container">
+                        {useNew ? (
+                            <button id="close-btn" onClick={handleClick}>&#10005;</button>
+                        ) : (
+                            <button id="new-btn" onClick={handleClick}>
+                                <p>&#43;</p>
+                                <p>Use New Credit Card</p>
+                            </button>
+                        )}
+
+                        {useNew && (
+                            <div className="new-card-form">
+                                <form onSubmit={handleConfirm}>
+                                    <fieldset>
+                                        <div className="form-select">
+                                            <p className="card-form-label">Select Card Type</p>
+                                            <select
+                                                value={cardType}
+                                                onChange={e => setCardType(e.target.value)}
+                                                required
+                                            >
+                                                <option value="" disabled> -- Select -- </option>
+                                                <option value="Master">Master</option>
+                                                <option value="Visa">Visa</option>
+                                                <option value="Discover">Discover</option>
+                                                <option value="AMEX">American Express</option>
+                                            </select>
+                                        </div>
+
+                                        <p className="card-form-label">Credit Card Number</p>
+                                        <input
+                                            type="text"
+                                            value={creditNum}
+                                            placeholder="Credit Card Number"
+                                            onChange={handleCardInput}
                                             required
-                                        >
-                                            <option value="" disabled> -- Select -- </option>
-                                            <option value="master">Master</option>
-                                            <option value="visa">Visa</option>
-                                            <option value="discover">Discover</option>
-                                            <option value="amex">American Express</option>
-                                        </select>
-                                    </div>
+                                        />
 
-                                    <p className="form-label">Credit Card Number</p>
-                                    <input 
-                                        type="text" 
-                                        value={creditNum} 
-                                        placeholder="Credit Card Number" 
-                                        onChange={handleCardInput} 
-                                        required 
-                                    />
+                                        <p className="card-form-label">Expiration Date</p>
+                                        <input
+                                            type="month"
+                                            value={expDate}
+                                            onChange={(e) => setExpDate(e.target.value)}
+                                            required
+                                        />
 
-                                    <p className="form-label">Expiration Date</p>
-                                    <input 
-                                        type="month" 
-                                        value={expDate} 
-                                        onChange={(e) => setExpDate(e.target.value)} 
-                                        required 
-                                    />
+                                        <p className="card-form-label">CVV</p>
+                                        <input
+                                            type="number"
+                                            value={cvv}
+                                            placeholder="CVV"
+                                            onChange={handleCVV}
+                                            required
+                                        />
 
-                                    <p className="form-label">CVV</p>
-                                    <input 
-                                        type="number" 
-                                        value={cvv} 
-                                        placeholder="CVV" 
-                                        onChange={handleCVV} 
-                                        required 
-                                    />
+                                        <button type="submit" id="confirm-btn" style={{ marginTop: "20px" }}>Save New Card and Pay</button>
+                                    </fieldset>
+                                </form>
+                            </div>
+                        )}
 
-                                    <button type="submit" id="submit-btn">Save</button>
-                                </fieldset>
-                            </form>
-                        </div>
+                        {cards.length > 0 && !useNew && (
+                            <div className="card-radio-container">
+                                {cards.map((c) => (
+                                    <label key={c.credit_card_num} className="card-radio-option">
+                                        <input
+                                            type="radio"
+                                            name="card"
+                                            value={c.credit_card_num}
+                                            checked={selectedCard === c.credit_card_num}
+                                            onChange={(e) => setSelectedCard(e.target.value)}
+                                        />
+                                        {c.card_type} ••••{c.credit_card_num.slice(-4)}
+                                    </label>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="divider" />
+                    <div className="item-container">
+                        <p className="item-label">Total Due</p>
+                        <p />
+                        <p style={{ justifySelf: "flex-end" }}>${total_price}</p>
+                    </div>
+
+                    {!useNew && cards.length > 0 && (
+                        <button id="confirm-btn" onClick={handleConfirm}>Confirm and Pay</button>
                     )}
                 </div>
-
-
-                    
-                <div className="divider" />
-                <div className="item-container">
-                    <p className="item-label">Total Due</p>
-                    <p />
-                    <p>${total_price}</p>
-                </div>
-
-                {!useNew && cards.length > 0 && (
-                    <button className="confirm-btn" onClick={handleConfirm}>Confirm and Pay</button>
-                )}
             </div>
-        </div>
         </>
     );
 };
