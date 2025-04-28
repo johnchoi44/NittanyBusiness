857a3376ae8618215bc91f3628dee0a37fa7ca1d
Khavab <shiblidnomani@gmail.com>
2025-04-28 04:37:50 -0400

    Profile overflow set

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -90,7 +90,7 @@ const Profile = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
+
         if (userType === "buyer") {
             try {
                 const res = await axios.put("http://localhost:8080/update-buyer", {
@@ -123,176 +123,178 @@ const Profile = () => {
     return (
         <div className="profile-container">
             <NavBar />
-            <div className="profile-background" />
+            <div className="profile-overflow">
+                <div className="profile-background" />
 
-            <div className="profile-contents">
-                <div className="profile-box">
-                    <div>
-                        <p id="profile-title">{userInfo.name}</p>
-                        <p id="profile-sub-title">{userEmail}</p>
+                <div className="profile-contents">
+                    <div className="profile-box">
+                        <div>
+                            <p id="profile-title">{userInfo.name}</p>
+                            <p id="profile-sub-title">{userEmail}</p>
+                        </div>
+                        <p />
+                        <button id="profile-btn" type="submit" onClick={handleSubmit}>Save Changes</button>
                     </div>
-                    <p />
-                    <button id="profile-btn" type="submit" onClick={handleSubmit}>Save Changes</button>
-                </div>
-                <form className="profile-form">
-                    <section className="profile-section">
-                        <p className="profile-label">Password Information</p>
-                        <p />
-                        <p />
-
-                        <div>
-                            <p className="profile-sub-label">Password</p>
-                            <input
-                                className="profile-input"
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Enter new password"
-                            />
-                        </div>
-                    </section>
-
-                    <section className="profile-section">
-                        <p className="profile-label">Address Information</p>
-                        <p />
-                        <p />
-
-                        <div>
-                            <p className="profile-sub-label">Street</p>
-                            <input
-                                className="profile-input"
-                                type="text"
-                                name="street"
-                                value={rawStreet}
-                                onChange={e => setRawStreet(e.target.value)}
-                                onBlur={handleStreetBlur}
-                            />
-                        </div>
-                        <p />
-                        <div>
-                            <p className="profile-sub-label">City</p>
-                            <input
-                                className="profile-input"
-                                type="text"
-                                name="city"
-                                value={userInfo.city}
-                                onChange={handleChange}
-                            />
-                        </div>
-
-                        <div>
-                            <p className="profile-sub-label">State</p>
-                            <input
-                                className="profile-input"
-                                type="text"
-                                name="state"
-                                value={userInfo.state}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <p />
-                        <div>
-                            <p className="profile-sub-label">Zipcode</p>
-                            <input
-                                className="profile-input"
-                                type="number"
-                                name="zipcode"
-                                value={userInfo.zipcode}
-                                onChange={handleChange}
-                            />
-                        </div>
-                    </section>
-
-                    {userType === "buyer" && (
-                        <section className="profile-section-card">
-                            <p className="profile-label">Financial Information</p>
-                            <p />
-                            <p />
+                    <form className="profile-form">
+                        <section className="profile-section">
+                            <p className="profile-label">Password Information</p>
                             <p />
                             <p />
 
                             <div>
-                                <p className="profile-sub-label">Card Type</p>
-                                <select
-                                    name="card_type"
-                                    onChange={handleChange}
-                                    required
-                                >
-                                    <option value="" disabled> -- Select -- </option>
-                                    <option value="master">Master</option>
-                                    <option value="visa">Visa</option>
-                                    <option value="discover">Discover</option>
-                                    <option value="amex">American Express</option>
-                                </select>
-                            </div>
-                            <p />
-                            <div>
-                                <p className="profile-sub-label">Credit Card Number</p>
+                                <p className="profile-sub-label">Password</p>
                                 <input
                                     className="profile-input"
-                                    name="credit_card_num"
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder="Enter new password"
+                                />
+                            </div>
+                        </section>
+
+                        <section className="profile-section">
+                            <p className="profile-label">Address Information</p>
+                            <p />
+                            <p />
+
+                            <div>
+                                <p className="profile-sub-label">Street</p>
+                                <input
+                                    className="profile-input"
                                     type="text"
-                                    inputMode="numeric"
-                                    value={userInfo.credit_card_num}
+                                    name="street"
+                                    value={rawStreet}
+                                    onChange={e => setRawStreet(e.target.value)}
+                                    onBlur={handleStreetBlur}
+                                />
+                            </div>
+                            <p />
+                            <div>
+                                <p className="profile-sub-label">City</p>
+                                <input
+                                    className="profile-input"
+                                    type="text"
+                                    name="city"
+                                    value={userInfo.city}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            <div>
+                                <p className="profile-sub-label">State</p>
+                                <input
+                                    className="profile-input"
+                                    type="text"
+                                    name="state"
+                                    value={userInfo.state}
                                     onChange={handleChange}
                                 />
                             </div>
                             <p />
                             <div>
-                                <p className="profile-sub-label">CVV</p>
+                                <p className="profile-sub-label">Zipcode</p>
                                 <input
                                     className="profile-input"
-                                    name="security_code"
-                                    value={userInfo.security_code}
+                                    type="number"
+                                    name="zipcode"
+                                    value={userInfo.zipcode}
                                     onChange={handleChange}
                                 />
                             </div>
                         </section>
-                    )}
 
-                    {userType === "seller" && (
-                        <section className="profile-section-card">
-                            <p className="profile-label">Financial Information</p>
-                            <p />
-                            <p />
-                            <p />
-                            <p />
+                        {userType === "buyer" && (
+                            <section className="profile-section-card">
+                                <p className="profile-label">Financial Information</p>
+                                <p />
+                                <p />
+                                <p />
+                                <p />
 
-                            <div>
-                                <p className="profile-sub-label">Routing Number</p>
-                                <input
-                                    className="profile-input"
-                                    name="bank_routing_number"
-                                    value={userInfo.bank_routing_number}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <p />
-                            <div>
-                                <p className="profile-sub-label">Account Number</p>
+                                <div>
+                                    <p className="profile-sub-label">Card Type</p>
+                                    <select
+                                        name="card_type"
+                                        onChange={handleChange}
+                                        required
+                                    >
+                                        <option value="" disabled> -- Select -- </option>
+                                        <option value="master">Master</option>
+                                        <option value="visa">Visa</option>
+                                        <option value="discover">Discover</option>
+                                        <option value="amex">American Express</option>
+                                    </select>
+                                </div>
+                                <p />
+                                <div>
+                                    <p className="profile-sub-label">Credit Card Number</p>
+                                    <input
+                                        className="profile-input"
+                                        name="credit_card_num"
+                                        type="text"
+                                        inputMode="numeric"
+                                        value={userInfo.credit_card_num}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <p />
+                                <div>
+                                    <p className="profile-sub-label">CVV</p>
+                                    <input
+                                        className="profile-input"
+                                        name="security_code"
+                                        value={userInfo.security_code}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                            </section>
+                        )}
 
-                                <input
-                                    className="profile-input"
-                                    name="bank_account_number"
-                                    value={userInfo.bank_account_number}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <p />
-                            <div>
-                                <p className="profile-sub-label">Balance</p>
-                                <input
-                                    className="profile-input"
-                                    name="balance"
-                                    value={"$ " + String(userInfo.balance)}
-                                    disabled
-                                />
-                            </div>
-                        </section>
-                    )}
-                </form>
+                        {userType === "seller" && (
+                            <section className="profile-section-card">
+                                <p className="profile-label">Financial Information</p>
+                                <p />
+                                <p />
+                                <p />
+                                <p />
+
+                                <div>
+                                    <p className="profile-sub-label">Routing Number</p>
+                                    <input
+                                        className="profile-input"
+                                        name="bank_routing_number"
+                                        value={userInfo.bank_routing_number}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <p />
+                                <div>
+                                    <p className="profile-sub-label">Account Number</p>
+
+                                    <input
+                                        className="profile-input"
+                                        name="bank_account_number"
+                                        value={userInfo.bank_account_number}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <p />
+                                <div>
+                                    <p className="profile-sub-label">Balance</p>
+                                    <input
+                                        className="profile-input"
+                                        name="balance"
+                                        value={"$ " + String(userInfo.balance)}
+                                        disabled
+                                    />
+                                </div>
+                            </section>
+                        )}
+                        <div className="profile-spacer" />
+                    </form>
+                </div>
             </div>
-
         </div >
     );
 };
